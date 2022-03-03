7f84c57397125cb3543987bd7b8ec544bab17a27
Prem Sichanugrist <s@sikac.hu>
2022-03-03 20:43:05 +0900

    Stop loading datetime precision patch on Rails 7

--- lib/cookpad_mysql_defaults.rb
+++ lib/cookpad_mysql_defaults.rb
@@ -1,6 +1,8 @@
 require "cookpad_mysql_defaults/mysql_table_default_options"
-require "cookpad_mysql_defaults/table_definition"
-require "cookpad_mysql_defaults/schema_statements"
 
-module CookpadMysqlDefaults
+# Only load datetime precision patch on Active Record 6.1 or below as
+# datetime(6) is now a default. See https://github.com/rails/rails/pull/42297
+if ActiveRecord::VERSION::MAJOR < 7
+  require "cookpad_mysql_defaults/table_definition"
+  require "cookpad_mysql_defaults/schema_statements"
 end
